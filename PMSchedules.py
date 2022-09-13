dfd5be7213e3367706964123fd914c73633823f6
FillGee <pgreyson@d118.org>
2022-09-13 15:03:17 -0500

    did further testing. still need to figure out ftp?

--- PMSchedules.py
+++ PMSchedules.py
@@ -1,7 +1,7 @@
 # Script to export all classes for students in the current term
 # Data Export Manager in PS only lets you choose the entire year term,
 # Which is an issue especially for HS students who have different periods per semester
-
+# Lots of debug / error catching because I had a lot of issues and wanted to narrow down which sections threw errors
 
 # importing module
 import oracledb
@@ -24,14 +24,17 @@ with oracledb.connect(user=un, password=pw, dsn=cs) as con: # create the connect
             print('SchoolID,Course Number,Section ID,Section Number,Expression,Student ID,Teacher Number,Teacher DCID', file=outputfile) #print header line in output file
             outputLog = open('schedule_log.txt', 'w') #open a second file for the log output
             try:
+                print("Connection established: " + con.version, file=outputLog)
                 cur.execute('SELECT student_number, first_name, last_name, id, schoolid, enroll_status, dcid FROM students ORDER BY student_number DESC')
                 rows = cur.fetchall()  # fetchall() is used to fetch all records from result set and store the data from the query into the rows variable
                 today = datetime.now()  # get todays date and store it for finding the correct term later
+                # today = datetime.now() + timedelta(days = 120) #testing purposes
+                print("today = " + str(today))  # debug
                 print("today = " + str(today), file=outputLog)  # debug
 
                 for entrytuple in rows: #go through each entry (which is a tuple) in rows. Each entrytuple is a single student's data
                     try:
-                        print(entrytuple)  # debug
+                        print(entrytuple, file=outputLog)  # debug
                         entry = list(entrytuple) #convert the tuple which is immutable to a list which we can edit. Now entry[] is an array/list of the student data
                         if not str(entry[1]) in badnames and not str(entry[2]) in badnames: #check first and last name against array of bad names, only print if both come back not in it
                             idNum = int(entry[0]) #what we would refer to as their "ID Number" aka 6 digit number starting with 22xxxx or 21xxxx
@@ -54,7 +57,7 @@ with oracledb.connect(user=un, password=pw, dsn=cs) as con: # create the connect
                                         if ((termEntry[1] - timedelta(days=2) < today) and (termEntry[2] + timedelta(days=1) > today)):
                                             termid = str(termEntry[0])
                                             termDCID = str(termEntry[4])
-                                            print("Found good term for student " + str(idNum) + ": " + termid + " | " + termDCID)
+                                            # print("Found good term for student " + str(idNum) + ": " + termid + " | " + termDCID)
                                             print("Found good term for student " + str(idNum) + ": " + termid + " | " + termDCID, file=outputLog)
                                             # now for each term that is valid, do a query for all their courses and start processing them
                                             try:
@@ -84,7 +87,8 @@ with oracledb.connect(user=un, password=pw, dsn=cs) as con: # create the connect
                                                     teacherName = str(teacherInfo[0][1])
 
                                                     #print out all the info for the class entry
-                                                    print(schoolID + '\t' + courseNum + '\t' + sectionID + '\t' + sectionNum + '\t' + expression + '\t' + str(idNum) + '\t' + teacherNum + '\t' + teacherDCID + '\t' + teacherName, file=outputfile)
+                                                    # print(schoolID + '\t' + courseNum + '\t' + sectionID + '\t' + sectionNum + '\t' + expression + '\t' + str(idNum) + '\t' + teacherNum + '\t' + teacherDCID + '\t' + teacherName, file=outputfile)
+                                                    print(schoolID + '\t' + courseNum + '\t' + sectionID + '\t' + sectionNum + '\t' + expression + '\t' + str(idNum) + '\t' + teacherNum + '\t' + teacherDCID, file=outputfile)
 
                                             except Exception as err:
                                                 print("Course error on " + str(idNum) + ": " + str(err))
